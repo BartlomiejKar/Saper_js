22555c21172d067ce159beaa594d1b9385da6518
Bartek Karelus <bartek.karelus@gmail.com>
2020-11-29 18:58:05 +0100

    add constructor to Cell.js

--- js/Cell.js
+++ js/Cell.js
@@ -1,3 +1,12 @@
 export class Cell {
-
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.isMine = false;
+        this.isFlagged = false;
+        this.value = 0;
+        this.isReveal = false;
+        this.selectpr = `[data-x="${this.x}"] [data-y="${this.y}"]`;
+        this.element = null;
+    }
 }
